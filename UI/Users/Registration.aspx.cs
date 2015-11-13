ff36093ccdd492f8ed9d94a3fae8cd9624ea44e7
Anettt <anett.92@gmail.com>
2015-11-13 12:34:50 +0100

    RegistrationUI
    Added forms and made some changes.

--- UI/Users/Registration.aspx.cs
+++ UI/Users/Registration.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using DataAccess;
+using Data;
+using System.Net.Http;
 
 namespace UI.Users
 {
@@ -13,5 +16,14 @@ namespace UI.Users
         {
 
         }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage wcfResponse = client.GetAsync("http://localhost:3729/HttpService.svc/MyHttpGetData/10").Result;
+            HttpContent stream = wcfResponse.Content;
+            var data = stream.ReadAsStringAsync();
+           // Label1.Text = data.Result;
+        }
+
     }
 }
