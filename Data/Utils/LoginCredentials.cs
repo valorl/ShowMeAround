7815219986ade12a89cc5fcc794549d1b020dab3
Valer Orlovsky <valer1212@gmail.com>
2015-11-24 13:31:38 +0100

    LoginCredentials.cs

---
+++ Data/Utils/LoginCredentials.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Utils
+{
+    [DataContract]
+    public class LoginCredentials
+    {
+        public LoginCredentials()
+        {
+
+        }
+
+
+        [DataMember]
+        public string Email { get; set; }
+        [DataMember]
+        public string Pwd { get; set; }
+    }
+}
