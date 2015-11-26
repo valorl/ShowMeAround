bec23ab1c2b81b71ad2d8ed7cb8e88cdd15f9be4
Mauro Monteiro <mauro_cesar_monteiro@hotmail.com>
2015-11-26 12:42:15 +0100

    City and Country DA

---
+++ DataAccess/CityDA.cs
@@ -0,0 +1,48 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class CityDA : IDataAccess<City>
+    {
+        private ShowMeAroundContext dbContext;
+
+        public CityDA()
+        {
+            dbContext = new ShowMeAroundContext();
+        }
+
+        public IEnumerable<City> GetAll()
+        {
+            using (var tempCtx = new ShowMeAroundContext())
+            {
+                var cities = tempCtx.City.ToList();
+                return cities;
+            }
+        }
+
+        public void Delete(City model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(City model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(City model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccess/CountryDA.cs
@@ -0,0 +1,49 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+
+{
+    public class CountryDA : IDataAccess<Country>
+    {
+        private ShowMeAroundContext dbContext;
+
+        public CountryDA()
+        {
+            dbContext = new ShowMeAroundContext();
+        }
+
+        public IEnumerable<Country> GetAll()
+        {
+            using (var tempCtx = new ShowMeAroundContext())
+            {
+                var countries = tempCtx.Country.ToList();
+                return countries;
+            }
+        }
+
+        public void Delete(Country model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Country model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Country model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
