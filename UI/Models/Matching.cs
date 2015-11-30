f5fad0a50ac8f9f6e8fe731ba6b8944719a87353
Anettt <anett.92@gmail.com>
2015-11-30 15:01:34 +0100

    Matching UI fixed

--- UI/Models/Matching.cs
+++ UI/Models/Matching.cs
@@ -6,26 +6,12 @@ namespace UI.Models
 {
     public class Matching
     {
-        public List<MatchingModel> Matches { get; set; }
-
-        public Matching()
-        {
-            Matches = new List<MatchingModel>();
-        }
-    }
-
-    public class MatchingModel
-    {
         public Match Match { get; set; }
-        public int Age
-        {
-            get
-            {
-                DateTime now = DateTime.Today;
-                int age = now.Year - Match.Guide.BirthDate.Year;
-                if (Match.Guide.BirthDate > now.AddYears(-age)) age--;
-                return age;
-            }
-        }
+        //public List<MatchingModel> Matches { get; set; }
+
+        //public Matching()
+        //{
+        //    Matches = new List<MatchingModel>();
+        //}
     }
 }
