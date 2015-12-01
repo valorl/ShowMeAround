7e43136c12fa79b338c4f2283108a7dbe2e0eb50
Miroslaw <mireksmoczy@gazeta.pl>
2015-12-01 20:31:01 +0100

    desktop client

---
+++ DesktopClient/Users.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace DesktopClient
+{
+    partial class Users
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CreateButton = new System.Windows.Forms.Button();
+            this.ListOfUsers = new System.Windows.Forms.ListBox();
+            this.EditButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.FillButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CreateButton
+            // 
+            this.CreateButton.Location = new System.Drawing.Point(135, 181);
+            this.CreateButton.Name = "CreateButton";
+            this.CreateButton.Size = new System.Drawing.Size(79, 30);
+            this.CreateButton.TabIndex = 0;
+            this.CreateButton.Text = "Create";
+            this.CreateButton.UseVisualStyleBackColor = true;
+            this.CreateButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ListOfUsers
+            // 
+            this.ListOfUsers.FormattingEnabled = true;
+            this.ListOfUsers.Location = new System.Drawing.Point(29, 23);
+            this.ListOfUsers.Name = "ListOfUsers";
+            this.ListOfUsers.Size = new System.Drawing.Size(392, 134);
+            this.ListOfUsers.TabIndex = 1;
+            this.ListOfUsers.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // EditButton
+            // 
+            this.EditButton.Location = new System.Drawing.Point(236, 181);
+            this.EditButton.Name = "EditButton";
+            this.EditButton.Size = new System.Drawing.Size(76, 30);
+            this.EditButton.TabIndex = 2;
+            this.EditButton.Text = "Edit";
+            this.EditButton.UseVisualStyleBackColor = true;
+            this.EditButton.Click += new System.EventHandler(this.EditButton_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(334, 181);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(87, 30);
+            this.DeleteButton.TabIndex = 3;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // FillButton
+            // 
+            this.FillButton.Location = new System.Drawing.Point(29, 181);
+            this.FillButton.Name = "FillButton";
+            this.FillButton.Size = new System.Drawing.Size(85, 30);
+            this.FillButton.TabIndex = 4;
+            this.FillButton.Text = "Fill";
+            this.FillButton.UseVisualStyleBackColor = true;
+            this.FillButton.Click += new System.EventHandler(this.FillButton_Click);
+            // 
+            // Users
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 281);
+            this.Controls.Add(this.FillButton);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.EditButton);
+            this.Controls.Add(this.ListOfUsers);
+            this.Controls.Add(this.CreateButton);
+            this.Name = "Users";
+            this.Text = "Users";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CreateButton;
+        private System.Windows.Forms.ListBox ListOfUsers;
+        private System.Windows.Forms.Button EditButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button FillButton;
+    }
+}
+
---
+++ DesktopClient/Users.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Service;
+
+namespace DesktopClient
+{
+    public partial class Users : Form
+    {
+        public Users()
+        {
+            InitializeComponent();
+            //UserDA.GetAll();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UserService.Create(User user);
+        }
+
+        private void EditButton_Click(object sender, EventArgs e)
+        {
+            //no action for now
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            //no action for now
+        }
+
+        private void FillButton_Click(object sender, EventArgs e)
+        {
+            UserService.GetAll();
+        }
+    }
+}
