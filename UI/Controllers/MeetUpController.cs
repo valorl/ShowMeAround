4cd05f22e732e3735f791c7a7c74e4ea5f02578f
Anettt <anett.92@gmail.com>
2015-12-03 16:42:58 +0100

    Request button, base for MeetUp page

---
+++ UI/Controllers/MeetUpController.cs
@@ -0,0 +1,22 @@
+﻿using Data;
+using Data.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UI.Helpers;
+using Utilities;
+
+namespace UI.Controllers
+{
+    public class MeetUpController : Controller
+    {
+        // GET: Matching
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
