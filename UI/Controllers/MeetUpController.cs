87aa4c622d199b26953bf146d161219a8255da0a
Anettt <anett.92@gmail.com>
2015-12-04 21:05:36 +0100

    MeetUp UI
    (not ready yet)

--- UI/Controllers/MeetUpController.cs
+++ UI/Controllers/MeetUpController.cs
@@ -15,7 +15,38 @@ namespace UI.Controllers
         // GET: Matching
         public ActionResult Index()
         {
+            //Check if user is logged in
+            if (!PageAuthorization.Authorize()) return RedirectToAction("Login", "Users");
 
+            //User user = new User();
+            //user.Id = 72;
+            var user = (User)Session["logged_in_user_obj"];
+
+
+            var client = new SMARestClient("MeetUpService.svc");
+            var meetupContent = client.Get<List<MeetUp>>("meetups/").ToList();
+            List<MeetUp> received = new List<MeetUp>();
+            List<MeetUp> sent = new List<MeetUp>();
+
+            foreach (var item in meetupContent)
+            {
+                if (item.Guide.Id == user.Id)
+                {
+                    received.Add(item);
+                }
+            }
+
+            foreach (var item in meetupContent)
+            {
+                if (item.Traveler.Id == user.Id)
+                {
+                    sent.Add(item);
+                }
+            }
+
+
+            ViewBag.ReceivedRequest = received;
+            ViewBag.SentRequest= sent;
             return View();
         }
     }
